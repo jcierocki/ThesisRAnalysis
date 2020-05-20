35922f19d271bf646612786e0cef87c07412b4bc
Jakub Cierocki <jcierocki@jcierocki-notebook.home>
2020-05-20 10:02:30 +0200

    Added new column to retrived data.

--- download.R
+++ download.R
@@ -15,8 +15,10 @@ df <- row_list %>% map_dfr(function(r) {
   node_list <- html_nodes(r, "td")
   county_name <- html_node(node_list[[2]], "a") %>% html_attr("href")
   numb_of_citizens <- node_list[[4]] %>% html_text() %>% str_remove_all(" ") %>% as.integer
+  urbanization_rate <- node_list[[6]] %>% html_text() %>% 
+    str_remove("%") %>% str_replace(",", ".") %>% str_replace("-", "0") %>% as.numeric
   
-  tibble(county_name, numb_of_citizens)
+  tibble(county_name, numb_of_citizens, urbanization_rate = urbanization_rate/100)
 })
 
 df %>% write_csv2("data/powiaty.csv")
